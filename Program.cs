9bf402eda2693f318afaefb36654283b3d40f21a
Maria <saskedrawings@gmail.com>
2026-09-18 22:34:37 -0300

    Tarefa realizada S2

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("|Média Aritmética:|");
+double numero1, numero2, numero3, média; 
+
+Console.WriteLine("Digite seu primeiro número: ");
+numero1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite seu segundo número: ");
+numero2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite seu terceiro número: ");
+numero3 = Convert.ToDouble(Console.ReadLine());
+
+média = (numero1 + numero2 + numero3) /3;
+
+Console.WriteLine($"A sua média é igual à: {média:N2}");
+
+
